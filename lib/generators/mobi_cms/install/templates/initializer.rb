3af90925a00cf7914bc616ce1a39be371989ab5e
M <mpatel.ror@gmail.com>
2013-01-24 18:05:10 +0530

    add mobi_cms configuration options

--- lib/generators/mobi_cms/install/templates/initializer.rb
+++ lib/generators/mobi_cms/install/templates/initializer.rb
@@ -1,3 +1,5 @@
 MobiCms.user_class = "<%= user_class %>"
-MobiCms.admin_user = false
-MobiCms.sign_in_path = "/auth/session/new"
+MobiCms.admin_user = :is_admin?
+MobiCms.sign_in_path = :auth_sign_in
+MobiCms.user_authenticate_method = :mobi_auth_authorize_user!
+MobiCms.login_user_email_helper = :auth_email
